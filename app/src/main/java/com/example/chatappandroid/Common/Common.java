f48aa6b8767f961be6a05ef700f9acea7ca65327
Jonathan Blas <cesar.bejucos.2014@gmail.com>
2021-05-16 17:18:35 -0500

    Listado de todos los chats disponibles, layouts, clases, parte 1.

--- app/src/main/java/com/example/chatappandroid/Common/Common.java
+++ app/src/main/java/com/example/chatappandroid/Common/Common.java
@@ -3,6 +3,7 @@ package com.example.chatappandroid.Common;
 import com.example.chatappandroid.Model.UserModel;
 
 public class Common {
+    public static final String CHAT_LIST_REFERENCE = "ChatList";
     public static UserModel currentUser = new UserModel();
     public static final String USER_REFERENCES = "people";
 }
