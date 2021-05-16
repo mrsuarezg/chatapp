5c866374ae3c7ba722f8b5f10d9e257cfaf89f1b
Jonathan Blas <cesar.bejucos.2014@gmail.com>
2021-05-15 19:02:20 -0500

    Registro exitoso de usuarios de prueba en la base de FireBase

---
+++ app/src/main/java/com/example/chatappandroid/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.chatappandroid.Common;
+
+import com.example.chatappandroid.Model.UserModel;
+
+public class Common {
+    public static UserModel currentUser = new UserModel();
+    public static final String USER_REFERENCES = "people";
+}
---
+++ app/src/main/java/com/example/chatappandroid/Model/UserModel.java
@@ -0,0 +1,68 @@
+package com.example.chatappandroid.Model;
+
+public class UserModel {
+    private String uid,fistName,lastName,phone,bio;
+    private double birthDate;
+    private boolean gender;
+
+    public UserModel(){
+        birthDate = System.currentTimeMillis();
+        gender = true;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public double getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(double birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+}
