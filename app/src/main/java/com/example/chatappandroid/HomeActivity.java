a2ff93e7cb89cdcf1a4f054276f073474645dd4b
Jonathan Blas <cesar.bejucos.2014@gmail.com>
2021-05-16 14:37:02 -0500

    Estilos TableLayout Selector, Themes, Values

--- app/src/main/java/com/example/chatappandroid/HomeActivity.java
+++ app/src/main/java/com/example/chatappandroid/HomeActivity.java
@@ -1,14 +1,63 @@
 package com.example.chatappandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.chatappandroid.Adapter.MyViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class HomeActivity extends AppCompatActivity {
+    @BindView(R.id.tabDat)
+    TabLayout tabLayout;
+    @BindView(R.id.view_pager)
+    ViewPager2 viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+        setupViewPager();
+    }
+
+    private void setupViewPager(){
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(new MyViewPagerAdapter(getSupportFragmentManager(), new Lifecycle() {
+
+            @Override
+            public void addObserver(@NonNull LifecycleObserver observer) {
+
+            }
+
+            @Override
+            public void removeObserver(@NonNull LifecycleObserver observer) {
+
+            }
+
+            @NonNull
+            @Override
+            public State getCurrentState() {
+                return null;
+            }
+        }));
+        new TabLayoutMediator(tabLayout,viewPager,(tab, position)->{
+            if(position == 0)
+                tab.setText("Chat");
+            else
+                tab.setText("People");
+        }).attach();
+
+    }
+
+    private void init(){
+        ButterKnife.bind(this);
     }
 }
