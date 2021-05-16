6e3bd1f5b542540ad43c5fcfb9a7f9d24828c683
Jonathan Blas <cesar.bejucos.2014@gmail.com>
2021-05-16 14:32:56 -0500

    Fragmnet Chat y People para el LayoutMediator

---
+++ app/src/main/java/com/example/chatappandroid/Fragments/ChatViewModel.java
@@ -0,0 +1,7 @@
+package com.example.chatappandroid.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChatViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/chatappandroid/Fragments/PeopleViewModel.java
@@ -0,0 +1,7 @@
+package com.example.chatappandroid.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class PeopleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
